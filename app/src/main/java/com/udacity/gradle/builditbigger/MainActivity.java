5db1fd20cecb1a3345b759bfc610c06e372787e8
Nicholas Welna <welnanick@gmail.com>
2018-05-24 10:19:47 -0700

    master: Step 2 completed

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.nickwelna.javajokes.Joker;
+import com.nickwelna.jokeviews.JokeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,20 +16,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -36,15 +41,22 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
+
         }
 
         return super.onOptionsItemSelected(item);
+
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_LONG).show();
-    }
 
+        Intent intent = new Intent(this, JokeActivity.class);
+        intent.putExtra(JokeActivity.JOKE_EXTRA, joker.getJoke());
+
+        startActivity(intent);
+
+    }
 
 }
---
+++ jokeviews/src/main/java/com/nickwelna/jokeviews/JokeActivity.java
@@ -0,0 +1,29 @@
+package com.nickwelna.jokeviews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static final String JOKE_EXTRA = "joke";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        TextView jokeTextView = findViewById(R.id.joke_text_view);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        String joke = extras.getString(JOKE_EXTRA);
+
+        jokeTextView.setText(joke);
+
+    }
+
+}
